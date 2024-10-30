e4ab0d38fb9d0d98e4b2421ce4845dd95f0e5850
oleh.opalko <oopalko96@gmail.com>
2024-10-30 21:54:06 +0200

    Remove CompressedManifestStaticFilesStorage

--- butterfly_shop/settings.py
+++ butterfly_shop/settings.py
@@ -146,8 +146,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
