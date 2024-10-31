c818d04d0a768d016db1a5286c41cb218b6bdc66
oleh.opalko <oopalko96@gmail.com>
2024-10-31 18:35:05 +0200

    Hot fix

--- butterfly_shop/urls.py
+++ butterfly_shop/urls.py
@@ -52,4 +52,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
