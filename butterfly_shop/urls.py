2a8bc36dc592054f09931904c1e2ad0ae25fadae
oleh.opalko <oopalko96@gmail.com>
2024-11-03 16:36:30 +0200

    Revert "Added settings to s3 storages"
    This reverts commit a589f2d2497343c90ee1b6f2e4e169442ffd7aa3.

--- butterfly_shop/urls.py
+++ butterfly_shop/urls.py
@@ -49,6 +49,7 @@ urlpatterns = [
     path('contacts/', contact, name='contacts'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# else:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
