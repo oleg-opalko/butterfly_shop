cf5ef4c6d56b3eedaee82a5625b472bf62cb1b73
oleh.opalko <oopalko96@gmail.com>
2024-11-03 15:33:53 +0200

    Changed url

--- butterfly_shop/urls.py
+++ butterfly_shop/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
     path('contacts/', contact, name='contacts'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# else:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
